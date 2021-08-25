f3cc2721cf9ebf807a6b6aa229a69d302a296251
haseeb0300 <muhammadhaseebkhan96@gmail.com>
2021-08-25 12:38:18 +0500

    updated

--- src/pages/Questions/Frcs2AddQuestion.js
+++ src/pages/Questions/Frcs2AddQuestion.js
@@ -28,9 +28,16 @@ class Frcs1AddQuestion extends Component {
             totalorders: "",
             bookList: [],
             newBookList: [],
+            clickedBtn: '',
 
         };
 
+    }
+
+    onclickBtn = (value) => {
+        this.setState({
+            clickedBtn: value
+        })
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
@@ -62,6 +69,7 @@ class Frcs1AddQuestion extends Component {
         }
 
         return (
+            <div className="oral-clinical-sec">
            <div className="frcs2-section mt-4 ">
              <div className="container  p-0">
              <div  className="row">
@@ -97,9 +105,9 @@ class Frcs1AddQuestion extends Component {
     <div className="col-md-6 mt-4 anas">
         <p className="poppins_regular test-type">Viva Type  <label className="staric">*</label></p>
         
-        <Link to="/frcs2/oraltype"><button className="oral-btn">ORAL</button>
-        </Link>
-        <Link to="/frcs2/clinicaltype"><button className="clinical-btn">CLINICAL</button></Link>
+        <button className="oral-btn" onClick={() => this.onclickBtn('oral')}>ORAL</button>
+        
+        <button className="clinical-btn" onClick={() => this.onclickBtn('clinical')}>CLINICAL</button>
         
          
          </div>
@@ -109,6 +117,285 @@ class Frcs1AddQuestion extends Component {
 
              </div>
            </div>
+
+
+
+
+{this.state.clickedBtn === 'oral' &&
+           <div className="addquestion-section  mt-4 ">
+             <div className="container   p-0">
+             <div  className="row ">
+                 <div className="col-md-12">
+                     <div className="heading-sec">
+                         <p className="poppins_medium head-txt">Core Details</p>
+                     </div>
+                 </div>
+                 <hr className="horizontal-line"></hr>
+                 </div>
+
+                 <div className="row  p-0">
+<div className="col-md-6 anas  ">
+        <p className="poppins_regular  test-type">Test Type <label className="staric">*</label></p>
+         <input type="text" className="input-box1" placeholder="FRCS 2"></input>
+         </div>
+    <div className="col-md-6 anas">
+        <p className="poppins_regular test-type">Domain <label className="staric">*</label></p>
+         <select  className="input-box2">
+             <option>OV-ORAL Viva </option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+         </select>
+         </div>
+             </div>
+
+             <div className="row p-0">
+<div className="col-md-6 anas ">
+        {/* <p className="poppins_regular test-type">Test Type <label className="staric">*</label></p>
+         <input type="text" className="input-box1" placeholder="FRCS 1"></input> */}
+         </div>
+    <div className="col-md-6 anas">
+    <p className="poppins_regular test-type">Domain <label className="staric">*</label></p>
+         <select  className="input-box2">
+             <option>BSD-1    -    Anatomy</option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+         </select>
+         
+         </div>
+             </div>
+
+
+             <div  className="row">
+                 <div className="col-md-12">
+                     <div className="heading-sec">
+                         <p className="poppins_medium head-txt">Question Details</p>
+                     </div>
+                 </div>
+                 <hr className="horizontal-line"></hr>
+                 </div>
+
+
+                 <div className="row p-0 mt-3">
+<div className="col-md-12 anas mt-2 ">
+        <p className="poppins_regular test-type">Scenario  <label className="staric">*</label></p>
+        <textarea type="text" className="question-box-full" placeholder="Please Enter Scenario"></textarea>
+         </div>
+             </div>
+
+
+             <div className="row mt-4 p-0">
+    <div className="col-md-6 anas">
+        <p className="poppins_regular test-type">Add Image Resource <label className="staric">*</label></p>
+        <input type="text" className="input-box1" placeholder="Paste URL"></input>
+         </div>
+
+         <div className="col-md-6 anas1 ">
+        <div className="row gapping">
+            <div className="col-md-6"><button className="uparrow-btn">
+            <img src={uparrow}/> &nbsp;&nbsp;&nbsp;
+            <label className="poppins_extralight upload">Upload Image</label>
+        </button></div>
+            <div className="col-md-6 "><img src={tick}/> &nbsp;&nbsp;&nbsp;
+            <label className="poppins_extralight demo">demo_image.jpg</label> </div>
+        </div>
+         </div>
+             </div>
+
+
+             <div className="row p-0 mt-3">
+<div className="col-md-12 anas mt-2 ">
+        <p className="poppins_regular test-type">Question  <label className="staric">*</label></p>
+        <textarea type="text" className="question-box-full" placeholder="Please Enter Question"></textarea>
+         </div>
+             </div>
+
+
+             <div className="row p-0 mt-3">
+<div className="col-md-12 anas mt-2 ">
+        <p className="poppins_regular test-type">Answer <label className="staric">*</label></p>
+        <textarea type="text" className="question-box-full" placeholder="Please Enter Answer"></textarea>
+         </div>
+             </div>
+
+
+             <div className="row p-0 mt-3">
+<div className="col-md-12 anas mt-2 ">
+        <p className="poppins_regular test-type">Point for Discussion  <label className="staric">*</label></p>
+        <textarea type="text" className="question-box-full" placeholder="Please Enter Point for Discussion "></textarea>
+         </div>
+             </div>
+
+
+             <div className="col-md-12 ">
+    <div className="save-add">
+        <button className="poppins_light save add-more">Add More Quetions</button>
+    </div>
+</div>
+
+             <div className="col-md-12 ">
+    <div className="save-add">
+        <button className="poppins_light save">Save & Add</button>
+    </div>
+</div>
+
+
+<div className="col-md-12 mt-4 ">
+    <div className="reset">
+        <p className="poppins_light reset-field">Reset the feild</p>
+    </div>
+</div>
+
+
+<div className="col-md-12 mt-4 ">
+    <div className="delete">
+        <p className="poppins_light delete-question">Delete Question</p>
+    </div>
+</div>
+             </div>
+           </div>
+
+}
+
+{this.state.clickedBtn === 'clinical' && 
+
+           <div className="addquestion-section  mt-4 ">
+             <div className="container   p-0">
+             <div  className="row">
+                 <div className="col-md-12">
+                     <div className="heading-sec">
+                         <p className="poppins_medium head-txt">Core Details</p>
+                     </div>
+                 </div>
+                 <hr className="horizontal-line"></hr>
+                 </div>
+
+                 <div className="row p-0">
+<div className="col-md-6 anas ">
+        <p className="poppins_regular test-type">Test Type <label className="staric">*</label></p>
+         <input type="text" className="input-box1" placeholder="FRCS 2"></input>
+         </div>
+    <div className="col-md-6 anas">
+        <p className="poppins_regular test-type">Domain <label className="staric">*</label></p>
+         <select  className="input-box2">
+             <option>CV    -   Clinical Viva </option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+         </select>
+         </div>
+             </div>
+
+             <div className="row p-0">
+<div className="col-md-6 anas ">
+        {/* <p className="poppins_regular test-type">Test Type <label className="staric">*</label></p>
+         <input type="text" className="input-box1" placeholder="FRCS 1"></input> */}
+         </div>
+    <div className="col-md-6 anas">
+    <p className="poppins_regular test-type">Clinical Viva Type <label className="staric">*</label></p>
+         <select  className="input-box2">
+             <option>GSC-SC-1    -   General Surgery (Long) </option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+             <option>SD-1-Professional Behavior & Leader....</option>
+         </select>
+         
+         </div>
+             </div>
+
+
+             <div  className="row">
+                 <div className="col-md-12">
+                     <div className="heading-sec">
+                         <p className="poppins_medium head-txt">Question Details</p>
+                     </div>
+                 </div>
+                 <hr className="horizontal-line"></hr>
+                 </div>
+
+
+                 {/* <div className="row p-0 mt-3">
+<div className="col-md-12 anas mt-2 ">
+        <p className="poppins_regular test-type">Scenario  <label className="staric">*</label></p>
+        <textarea type="text" className="question-box-full" placeholder="Please Enter Scenario"></textarea>
+         </div>
+             </div> */}
+
+
+             <div className="row mt-4 p-0">
+    <div className="col-md-6 anas">
+        <p className="poppins_regular test-type">Add Image Resource <label className="staric">*</label></p>
+        <input type="text" className="input-box1" placeholder="Paste URL"></input>
+         </div>
+
+         <div className="col-md-6 anas1 ">
+        <div className="row gapping">
+            <div className="col-md-6"><button className="uparrow-btn">
+            <img src={uparrow}/> &nbsp;&nbsp;&nbsp;
+            <label className="poppins_extralight upload">Upload Image</label>
+        </button></div>
+            <div className="col-md-6 "><img src={tick}/> &nbsp;&nbsp;&nbsp;
+            <label className="poppins_extralight demo">demo_image.jpg</label> </div>
+        </div>
+         </div>
+             </div>
+
+
+             <div className="row p-0 mt-3">
+<div className="col-md-12 anas mt-2 ">
+        <p className="poppins_regular test-type">Follow up Question <label className="staric">*</label></p>
+        <textarea type="text" className="question-box-full" placeholder="Please Enter Question"></textarea>
+         </div>
+             </div>
+
+
+             <div className="row p-0 mt-3">
+<div className="col-md-12 anas mt-2 ">
+        <p className="poppins_regular test-type">Answer  <label className="staric">*</label></p>
+        <textarea type="text" className="question-box-full" placeholder="Please Enter Answer"></textarea>
+         </div>
+             </div>
+
+
+             <div className="row p-0 mt-3">
+<div className="col-md-12 anas mt-2 ">
+        <p className="poppins_regular test-type">Suggest Reading / Video Link  <label className="staric">*</label></p>
+        <textarea type="text" className="question-box-full" placeholder="Suggest Reading / Video Link "></textarea>
+         </div>
+             </div>
+
+
+             {/* <div className="col-md-12 ">
+    <div className="save-add">
+        <button className="poppins_light save add-more">Add More Quetions</button>
+    </div>
+</div> */}
+
+             <div className="col-md-12 ">
+    <div className="save-add">
+        <button className="poppins_light save">Save & Add</button>
+    </div>
+</div>
+
+
+<div className="col-md-12 mt-4 ">
+    <div className="reset">
+        <p className="poppins_light reset-field">Reset the feild</p>
+    </div>
+</div>
+
+
+<div className="col-md-12 mt-4 ">
+    <div className="delete">
+        <p className="poppins_light delete-question">Delete Question</p>
+    </div>
+</div>
+             </div>
+           </div>
+           }
+           </div>
+
         )
     }
 
